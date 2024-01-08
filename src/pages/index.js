3982dca4f49615fe3d4f318215368bd6f7f59725
shariarhossain17 <shariarhossain23@gmail.com>
2024-01-08 19:43:17 +0600

    fetching news data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,28 @@
 import RootLayout from "@/Layout/RootLayout";
 import Banner from "@/components/ Banner/Banner";
 
-const HomePage = () => {
-
-
+const HomePage = ({allNews}) => {
+  console.log(allNews)
   return (
-  <>
-
-    <Banner/>
-  </>
+    <>
+      <Banner />
+    </>
   );
 };
 export default HomePage;
 
- HomePage.getLayout = function getLayout (page){
-  return (
-    <RootLayout>{page}</RootLayout>
-  )
- }
+HomePage.getLayout = function getLayout(page) {
+  return <RootLayout>{page}</RootLayout>;
+};
+
+export const getStaticProps = async () => {
+  const res = await fetch("http://localhost:5000/news");
+  const data = await res.json();
+
+  console.log(data);
+  return {
+    props: {
+      allNews: data,
+    },
+  };
+};
