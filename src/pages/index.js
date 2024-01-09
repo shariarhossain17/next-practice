dcf43e43a22b786675644e932eec6ab6ab9de7cb
shariarhossain17 <shariarhossain23@gmail.com>
2024-01-09 19:20:44 +0600

    fetching data using server side props

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,13 +22,23 @@ HomePage.getLayout = function getLayout(page) {
   return <RootLayout>{page}</RootLayout>;
 };
 
-export const getStaticProps = async () => {
+export const getServerSideProps = async () => {
   const res = await fetch("http://localhost:5000/news");
   const data = await res.json();
   return {
     props: {
       allNews: data,
-    },
-    revalidate:10,
+    }
   };
 };
+
+// export const getStaticProps = async () => {
+//   const res = await fetch("http://localhost:5000/news");
+//   const data = await res.json();
+//   return {
+//     props: {
+//       allNews: data,
+//     },
+//     revalidate:10,
+//   };
+// };
--- src/pages/news/[newsId].jsx
+++ src/pages/news/[newsId].jsx
@@ -1,11 +1,12 @@
 import RootLayout from "@/Layout/RootLayout";
+import NewsCard from "@/components/Allnews/NewsCard";
 
 const NewsDetails = ({singleNews}) => {
 // console.log(singleNews);
 
     return (
         <div>
-            <h1>{singleNews.title}</h1>
+          <NewsCard news={singleNews}></NewsCard>
         </div>
     );
 };
@@ -18,18 +19,18 @@ NewsDetails.getLayout = function getLayout (page) {
     return <RootLayout>{page}</RootLayout>
 }
 
-export const getStaticPaths = async () => {
-  const res = await fetch("http://localhost:5000/news");
-  const newses = await res.json();
+// export const getStaticPaths = async () => {
+//   const res = await fetch("http://localhost:5000/news");
+//   const newses = await res.json();
 
-  const paths = newses.map((news) => ({
-    params: { newsId: String(news?.id) },
-  }));
+//   const paths = newses.map((news) => ({
+//     params: { newsId: String(news?.id) },
+//   }));
 
 
-  return { paths, fallback: false };
-};
-export const getStaticProps = async (context) => {
+//   return { paths, fallback: false };
+// };
+export const getServerSideProps = async (context) => {
 
     const {params}=context
 
