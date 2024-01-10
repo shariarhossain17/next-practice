e657a6919aa2deb13556f2833225d3552f1b8be5
shariarhossain17 <shariarhossain23@gmail.com>
2024-01-10 15:17:06 +0600

    creata navbar

---
+++ app-route/src/components/shared/Navbar.js
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+const Navbar = () => {
+    return (
+        <div>
+        <nav className="flex justify-between items-center bg-black px-12 p-4">
+            <div>
+                <h1 className="text-4xl font-semibold text-white bg-gray-500 p-2 rounded-[5px] ">New paper</h1>
+            </div>
+            <div className="text-white">
+                <Link className="ml-8" href="/news">All News</Link>
+                <Link className="ml-8" href="/about">About</Link>
+                <Link className="ml-8" href="/contact">contact</Link>
+            </div>
+        </nav>
+    </div>
+    );
+};
+
+export default Navbar;
